085ef1cfc6b2a61f4047fcf9c7c40fd2b1a1c989
ostapkob <ostap666@yandex.ru>
2019-11-16 15:11:28 +1000

    ee ;

--- post.py
+++ post.py
@@ -18,7 +18,7 @@ data = {"password" : "super",
 head = {"Content-type": "application/json",
           "Accept": "text/plain"}
 
-for i in range(1, 12):
+for i in range(1, 12, 2):
     data['mechanism_id'] =i
     data['value']= round(random(),3)
     jdata = json.dumps(data)
