e14693e5ec42718b83def619fe65e6ab73e09cc4
ostapkob <ostapkob@gmail.com>
2020-01-09 00:27:14 +1000

    'fix_add_post2'

--- add_mechanisms.py
+++ add_mechanisms.py
@@ -4,57 +4,45 @@ from app import db
 from app.model import Mechanism, Post
 from functions import today_shift_date, add_post
 
-# db.create_all()
-# m = Mechanism(32046, company='nmtp', type='usm', model='PowerTrack', number=1, name='PowerTrack-1')
-# db.session.add(m)
-# m = Mechanism(32047, company='nmtp', type='usm', model='PowerTrack', number=2, name='PowerTrack-2')
-# db.session.add(m)
-# m = Mechanism(32711, company='nmtp', type='usm', model='Edge', number=3, name='Edge-3')
-# db.session.add(m)
-# m = Mechanism(32740, company='nmtp', type='usm', model='Edge', number=4, name='Edge-4')
-# db.session.add(m)
-# m = Mechanism(32770, company='nmtp', type='usm', model='Screen', number=5, name='Screen-5')
-# db.session.add(m)
-# m = Mechanism(32771, company='nmtp', type='usm', model='Screen', number=6, name='Screen-6')
-# db.session.add(m)
-# m = Mechanism(32772, company='nmtp', type='usm', model='Screen', number=7, name='Screen-7')
-# db.session.add(m)
-# m = Mechanism(32773, company='nmtp', type='usm', model='Screen', number=8, name='Screen-8')
-# db.session.add(m)
-# m = Mechanism(32941, company='nmtp', type='usm', model='Electric', number=9, name='Electric-9')
-# db.session.add(m)
-# m = Mechanism(32942, company='nmtp', type='usm', model='Electric', number=10, name='Electric-10')
-# db.session.add(m)
-# m = Mechanism(33287, company='nmtp', type='usm', model='Edge', number=11, name='Edge-11')
-# db.session.add(m)
-# m = Mechanism(32777, company='nmtp', type='sennebogen', model='860', number=1, name='Sennebogen-1')
-# db.session.add(m)
-# db.session.commit()
-# a = [mech.id for mech in Mechanism.query.all()]
-# print(a)
+db.create_all()
+m = Mechanism(32046, company='nmtp', type='usm', model='PowerTrack', number=1, name='PowerTrack-1')
+db.session.add(m)
+m = Mechanism(32047, company='nmtp', type='usm', model='PowerTrack', number=2, name='PowerTrack-2')
+db.session.add(m)
+m = Mechanism(32711, company='nmtp', type='usm', model='Edge', number=3, name='Edge-3')
+db.session.add(m)
+m = Mechanism(32740, company='nmtp', type='usm', model='Edge', number=4, name='Edge-4')
+db.session.add(m)
+m = Mechanism(32770, company='nmtp', type='usm', model='Screen', number=5, name='Screen-5')
+db.session.add(m)
+m = Mechanism(32771, company='nmtp', type='usm', model='Screen', number=6, name='Screen-6')
+db.session.add(m)
+m = Mechanism(32772, company='nmtp', type='usm', model='Screen', number=7, name='Screen-7')
+db.session.add(m)
+m = Mechanism(32773, company='nmtp', type='usm', model='Screen', number=8, name='Screen-8')
+db.session.add(m)
+m = Mechanism(32941, company='nmtp', type='usm', model='Electric', number=9, name='Electric-9')
+db.session.add(m)
+m = Mechanism(32942, company='nmtp', type='usm', model='Electric', number=10, name='Electric-10')
+db.session.add(m)
+m = Mechanism(33287, company='nmtp', type='usm', model='Edge', number=11, name='Edge-11')
+db.session.add(m)
+m = Mechanism(32777, company='nmtp', type='sennebogen', model='860', number=1, name='Sennebogen-1')
+db.session.add(m)
+db.session.commit()
+a = [mech.id for mech in Mechanism.query.all()]
+print(a)
 
-m_id = 32047
-now = datetime.utcnow() - timedelta(seconds=6200)
+# m_id = 32047
+# now = datetime.utcnow() - timedelta(seconds=6200)
 # p = Post(value=0.99, latitude=23, longitude=12, mechanism_id=m_id, timestamp=now)
-post = Post(value=0.99, latitude=23, longitude=12, mechanism_id=m_id) #, timestamp=now)
-last = db.session.query(Post).filter(Post.mechanism_id==post.mechanism_id).order_by(Post.timestamp.desc()).first()
+# post = Post(value=0.99, latitude=23, longitude=12, mechanism_id=m_id) #, timestamp=now)
+# last = db.session.query(Post).filter(Post.mechanism_id==post.mechanism_id).order_by(Post.timestamp.desc()).first()
 
 # db.session.add(post)
 # db.session.commit()
 
-def add_post(post):
-    ''' I use it fix because arduino sometimes accumulates an extra minute '''
-    dt_seconds =  (post.timestamp -last.timestamp).seconds
-    if dt_seconds < 200: # whatever the difference is not big
-        last_minute =  last.timestamp.minute
-        post_minute =  post.timestamp.minute
-        dt_minutes = post_minute - last_minute
-        if dt_minutes == 2 or dt_minutes == -58:
-            post.timestamp -= timedelta(seconds=30)
-    db.session.add(post)
-    db.session.commit()
-
-add_post(post)
+# add_post(post)
 
 
 
