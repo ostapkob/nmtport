a2071df569f72497933806f234eeb45eec4eb764
ostapkob <ostapkob@gmail.com>
2020-01-17 19:58:22 +1000

    TeleBot2

--- bot.py
+++ bot.py
@@ -42,31 +42,28 @@ tests = ( [[1, -1, -1, -1, -1], True],
 
 
 
-server_ip ="http://35.241.126.216"
+host ="http://35.241.126.216"
 type_mechanism = "usm"
 date_shift = "17.01.2020"
 shift = "1"
 API = f"/api/v1.0/get_data/{type_mechanism}/{date_shift}/{shift}"
 TOKEN = "977352466:AAEgH-c6FFFGbv71pSBP8hbtu9oSS6JrY48"
 amount_elements = 5
-data = requests.get(server_ip+API)
-mechanisms = json.loads(data.text)
-# pprint(mechanisms)
 bot = telebot.TeleBot(TOKEN)
 
-
-bot.send_message(226566335, 'start')
-
-
-for mech, data_mech in mechanisms.items():
-    data = data_mech['data']
-    name_mech = data_mech['name']
-    last_numbers = range(len(data)-amount_elements, len(data))
-    values_last_5_minutes = [data[str(num)]['value'] for num in last_numbers]
-    print(values_last_5_minutes)
-    if chech_values(values_last_5_minutes, -1):
-        bot.send_message(226566335, name_mech)
-        print("-1 -bo---->", name_mech)
-    if chech_values(values_last_5_minutes, 0):
-        bot.send_message(226566335, name_mech)
-        print("0 ----->", name_mech)
+while True:
+    data = requests.get(host+API)
+    mechanisms = json.loads(data.text)
+    for mech, data_mech in mechanisms.items():
+        data = data_mech['data']
+        name_mech = data_mech['name']
+        last_numbers = range(len(data)-amount_elements, len(data))
+        values_last_5_minutes = [data[str(num)]['value'] for num in last_numbers]
+        print(values_last_5_minutes)
+        if chech_values(values_last_5_minutes, -1):
+            bot.send_message(226566335, name_mech)
+            print("-1 -bo---->", name_mech)
+        if chech_values(values_last_5_minutes, 0):
+            bot.send_message(226566335, name_mech)
+            print("0 ----->", name_mech)
+    time.sleep(10)
