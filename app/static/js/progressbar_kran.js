6ce5875cb8df5eb777ee66cdf601dd1d6f6ba4f8
ostapkob <ostapkob@gmail.com>
2020-03-01 07:40:57 +1000

    add block kran

--- app/static/js/progressbar_kran.js
+++ app/static/js/progressbar_kran.js
@@ -19,7 +19,7 @@
         // },
 
         getData () {
-        this.$http.get('/api/v1.0/get_data/usm/'+this.date_shift+'/'+this.shift).then(response => {
+        this.$http.get('/api/v1.0/get_data/kran/'+this.date_shift+'/'+this.shift).then(response => {
             this.all_data = response.body
         }, response => {
             console.log('response err')
@@ -31,19 +31,19 @@
             const today = new Date();
             hour=today.getHours();
             if (hour>=8 && hour<20) {
-                this.date_shift= today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
                 this.shift=1;
             }
             else if (hour<8) {
-                this.date_shift= (today.getDate()-1)+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
                 this.shift=2;
+                today.setDate(today.getDate()-1);
             }
             else {
-                this.date_shift= today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
                 this.shift=2
             }
+            this.date_shift= today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
             // this.api=this.http[0]+this.date_shift+'/'+this.shift //think
-
+            console.log(this.date_shift);
+            console.log(hour);
             if (this.shift==1) {
                 this.hs = ["║",  "╵", "09",  "╵", "10",  "╵", "11",  "╵", "12",  "╵", "13",  "╵", "14",  "╵", "15",  "╵", "16",  "╵", "17",  "╵", "18",  "╵", "19",  "╵",  "║"] }
                 else {
