960a2d923b31de6724c480398cd4dde6b12dd85d
ostapkob <ostapkob.gmail.com>
2021-01-23 22:17:51 +1000

    new krans

--- middleware/add_mechanisms.py
+++ middleware/add_mechanisms.py
@@ -5,8 +5,23 @@ from psw import users
 db.create_all()
 from datetime import datetime, timedelta
 
-mech = [(4934,  'nmtp', 'kran', 'sokol', 20, 'кран-20'),
-        (14195, 'nmtp', 'kran', 'sokol', 8, 'кран-8'),
+mech = [
+        (22272, 'nmtp', 'kran', 'sokol', 36, 'кран-36'),
+        (22152, 'nmtp', 'kran', 'sokol', 40, 'кран-40'),
+        (13969, 'nmtp', 'kran', 'sokol', 32, 'кран-32'),
+        (6573,  'nmtp', 'kran', 'albatross', 25, 'кран-25'),
+        (14194, 'nmtp', 'kran', 'sokol', 11, 'кран-11'),
+        # (15512, 'nmtp', 'kran', 'sokol', 14, 'кран-14'),
+        # (15510, 'nmtp', 'kran', 'sokol', 16, 'кран-16'),
+        # (15511, 'nmtp', 'kran', 'sokol', 33, 'кран-33'),
+        # (4513,  'nmtp', 'kran', 'sokol', 47, 'кран-47'),
+        # (30271, 'nmtp', 'kran', 'sokol', 54, 'кран-54'),
+        # (30301, 'nmtp', 'kran', 'sokol', 4, 'кран-4'),
+        # (14195, 'nmtp', 'kran', 'sokol', 8, 'кран-8'),
+        # (13893, 'nmtp', 'kran', 'sokol', 12, 'кран-12'),
+        # (15125, 'nmtp', 'kran', 'sokol', 13, 'кран-13'),
+        # (4934,  'nmtp', 'kran', 'sokol', 20, 'кран-20'),
+        # (5908,  'nmtp', 'kran', 'sokol', 22, 'кран-22'),
         # (32046, 'nmtp', 'usm', 'PowerTrack', 1, 'PowerTrack-1'),
         # (32047, 'nmtp', 'usm', 'PowerTrack', 2, 'PowerTrack-2'),
         # (32711, 'nmtp', 'usm', 'Edge', 3, 'Edge-3'),
@@ -19,15 +34,11 @@ mech = [(4934,  'nmtp', 'kran', 'sokol', 20, 'кран-20'),
         # (32942, 'nmtp', 'usm', 'Electric', 10, 'Electric-10'),
         # (33287, 'nmtp', 'usm', 'Edge', 11, 'Edge-11'),
         # (32777, 'nmtp', 'sennebogen', '860', 1, 'Sennebogen-1'),
-        # (30301, 'nmtp', 'kran', 'sokol', 4, 'кран-4'),
-        # (13893, 'nmtp', 'kran', 'sokol', 12, 'кран-12'),
-        # (15125, 'nmtp', 'kran', 'sokol', 13, 'кран-13'),
-        # (5908,  'nmtp', 'kran', 'sokol', 22, 'кран-22'),
         ]
-
 
 for id, company, type, model, number, name in mech:
     db.session.add(Mechanism(id, company, type, model, number, name))
+    print(id, company, type, model, number, name)
 
 # for id, username, password in users:
 #     db.session.add(User(id, username, password))
