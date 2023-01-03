a761ba4f1bba247081f82db68f551c3f3e4874dd
ostapkob <ostapkob.gmail.com>
2023-01-03 21:01:18 +1000

    add from api-2

--- middleware/add_items_from_nginx.py
+++ middleware/add_items_from_nginx.py
@@ -52,6 +52,7 @@ table_usm.add_column("terminal", justify="right", style="blue")
 table_usm2 = Table(title="USM2")
 table_usm2.add_column("timestamp", justify="right", style="cyan", no_wrap=True)
 table_usm2.add_column("number",   style="magenta")
+table_usm2.add_column("mech_id",   style="magenta")
 table_usm2.add_column("pass",     justify="right", style="green")
 table_usm2.add_column("count",    justify="right", style="green")
 table_usm2.add_column("lever",   justify="right", style="blue")
@@ -94,7 +95,7 @@ def test_items_usm(items):
 
 def test_items_usm2(items):
     # timestamp, number, mechanism_id, passw, value, value2, value3, count, lat, lon, terminal = items
-    timestamp, number,  passw, count, lever, roll, rfid, flag, lat, lon, terminal= items
+    timestamp, number,  mechanism_id, passw, count, lever, roll, rfid, flag, lat, lon, terminal= items
     test_nums =  number, lever, roll, count, lat, lon, terminal 
     if not isinstance(timestamp, datetime):
         return False
@@ -105,46 +106,48 @@ def test_items_usm2(items):
     return True
 
 
-nginx_log = '/var/log/nginx/access.log'
+nginx_log = '/var/log/nginx/access.log' # today
+# nginx_log = '/var/log/nginx/access.log.1' # yesterday
 with open(nginx_log) as f:
     lines = f.readlines()
 
-time_start = datetime(2022, 8, 27, 8, 0, 0)
-time_finish = datetime(2022, 8, 27, 20, 0, 0)
+time_start = datetime(2022, 12, 8, 0, 0, 0)
+time_finish = datetime(2022, 12, 8, 8, 41, 59)
+
 for line_file in lines:
-    # if 'add_kran' in line_file:
-    #     line = line_file.split('&')
-    #     time = re.search(r'\[.*\]', line[0])[0]
-    #     time = time[1:21]
-    #     time = datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
-    #     if time<time_start or time>time_finish:
-    #         continue
-    #     number = line[0].split('=')[1].strip()
-    #     passw = line[1].split('=')[1].strip()
-    #     value = line[2].split('=')[1].strip()
-    #     count = line[3].split('=')[1].strip()
-    #     lat = line[4].split('=')[1].strip()
-    #     lon = line[5].split('=')[1].strip()
-    #     x = line[6].split('=')[1].strip()
-    #     y = line[7].split(' ')[0].split('=')[1].strip()
-    #     mechanism_id = dict_mechanisms_id_by_number['kran'][int(number)]
-    #     terminal = which_terminal('kran', number, lat, lon) 
-    #     timestamp=time-timedelta(hours=HOURS)
-    #     items = (timestamp,number, mechanism_id, passw, value, count, lat, lon, x, y, terminal)
-    #     if not test_items_kran(items):
-    #         print("Err kran in", items)
-    #         continue
-    #     table_kran.add_row(*[str(i) for i in items])
-    #     new_post = Post(
-    #                 timestamp=timestamp,
-    #                 mechanism_id=mechanism_id, 
-    #                 value=value,
-    #                 count=count,
-    #                 latitude=lat,
-    #                 longitude=lon,
-    #                 terminal=terminal,
-    #     )
-    #     db.session.add(new_post)
+#     if 'add_kran' in line_file:
+#         line = line_file.split('&')
+#         time = re.search(r'\[.*\]', line[0])[0]
+#         time = time[1:21]
+#         time = datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
+#         if time<time_start or time>time_finish:
+#             continue
+#         number = line[0].split('=')[1].strip()
+#         passw = line[1].split('=')[1].strip()
+#         value = line[2].split('=')[1].strip()
+#         count = line[3].split('=')[1].strip()
+#         lat = line[4].split('=')[1].strip()
+#         lon = line[5].split('=')[1].strip()
+#         x = line[6].split('=')[1].strip()
+#         y = line[7].split(' ')[0].split('=')[1].strip()
+#         mechanism_id = dict_mechanisms_id_by_number['kran'][int(number)]
+#         terminal = which_terminal('kran', number, lat, lon) 
+#         timestamp=time-timedelta(hours=HOURS)
+#         items = (timestamp,number, mechanism_id, passw, value, count, lat, lon, x, y, terminal)
+#         if not test_items_kran(items):
+#             print("Err kran in", items)
+#             continue
+#         table_kran.add_row(*[str(i) for i in items])
+#         new_post = Post(
+#                     timestamp=timestamp,
+#                     mechanism_id=mechanism_id, 
+#                     value=value,
+#                     count=count,
+#                     latitude=lat,
+#                     longitude=lon,
+#                     terminal=terminal,
+#         )
+#         db.session.add(new_post)
 
 
 
@@ -200,51 +203,52 @@ for line_file in lines:
 
 
     if 'add_usm_work?' in line_file:
+
+         # /api/v2.0/add_usm_work?number=12&passw=327a&count=70&bad=0&lever=1.00&roll=23&rfid=0000000000&flag=0&lat=42.814473&lon=132.890733 HTTP/1.1"
         line = line_file.split('&')
         time = re.search(r'\[.*\]', line[0])[0]
         time = time[1:21]
         timestamp = datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
-        if time_finish<timestamp<time_start:
+        if timestamp<time_start or timestamp>time_finish:
             continue
-        # mechanism_id = line[0].split('=')[1]
-        # print(line)
         number = line[0].split('=')[1]
         passw = line[1].split('=')[1]
         count = line[2].split('=')[1]
-        lever = line[3].split('=')[1]
-        roll = line[4].split('=')[1]
-        rfid = line[5].split('=')[1]
-        flag = line[6].split('=')[1]
-        lat = line[7].split('=')[1]
-        lon = line[8].split(' ')[0].split('=')[1]
-        # print(time, number, passw, count, lever, roll, rfid, lat, lon)
-        if not number=='13': 
-            continue
+        bad = line[3].split('=')[1]
+
+        lever = line[4].split('=')[1]
+        roll = line[5].split('=')[1]
+        rfid = line[6].split('=')[1]
+        flag = line[7].split('=')[1]
+        lat = line[8].split('=')[1]
+        lon = line[9].split(' ')[0].split('=')[1]
+        mechanism_id = dict_mechanisms_id_by_number['usm'][int(number)]
+        # if not number=='13':  #FIX
+        #     continue
         # if float(value)>0:
         #     value3=24
             
         terminal = which_terminal('usm', number, lat, lon) 
 
-        items = (timestamp, number,  passw, count, lever, roll, rfid, flag, lat, lon, terminal)
+        items = (timestamp, number, mechanism_id,  passw, count, lever, roll, rfid, flag, lat, lon, terminal)
         
         if not test_items_usm2(items):
-            # print("Err USM v2.0 in", items,  style="red")
             print("Err USM v2.0 in", items)
             continue
 
         table_usm2.add_row(*[str(i) for i in items])
-        # new_post = Post(
-        #             timestamp=timestamp,
-        #             mechanism_id=mechanism_id, 
-        #             value=value,
-        #             value2=value2,
-        #             value3=value3,
-        #             count=count,
-        #             latitude=lat,
-        #             longitude=lon,
-        #             terminal=terminal,
-        # )
-        # db.session.add(new_post)
+        new_post = Post(
+                    timestamp=timestamp,
+                    mechanism_id=mechanism_id, 
+                    value=lever,
+                    value2=1, # not need
+                    value3=roll,
+                    count=count,
+                    latitude=lat,
+                    longitude=lon,
+                    terminal=terminal,
+        )
+        db.session.add(new_post)
 
 
 # console.print(table_kran)
@@ -253,5 +257,5 @@ console.print(table_usm2)
 
 
 
-# print('FINISH')
-# db.session.commit()
+print('FINISH')
+db.session.commit()
